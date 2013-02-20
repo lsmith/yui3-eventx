c9dd0917916729bb0f2945634c58199eb0bf6ed7
Luke Smith <lsmith@lucassmith.name>
2013-02-19 21:16:49 -0800

    WIP. Rewriting all the things
    The beginnings of Y.Event are in place, the default event code to subscribe to
    DOM events. Lots yet to be done.

--- event-dom.js
+++ event-dom.js
@@ -14,12 +14,272 @@ var EventTarget = Y.EventTarget,
     YEventDOM,
     eventMgr;
     
-function DOMEventFacade(type, e) {
+function Subscription() {
+}
+
+Subscription.prototype = {
+};
+
+function DOMSubscription(target, type, capture) {
+}
+
+DOMSubscription.prototype = {
+};
+
+function DOMEventFacade() {
+}
+
+DOMEventFacade.prototype = {
+};
+
+// Y.Event gets the EventTarget interface, but...
+Y.Event = {
+    _subs: {},
+
+    _events: {
+        // Base event is dynamic to allow for dynamic DOM subscriptions such as
+        // `Y.Event.on('click:li.expandable', callback, '.tree')`
+        '@BASE': new Y.CustomEvent('@BASE', {
+            //TODO: simplified method implementations, no phases
+            Event: DOMEventFacade,
+            Subscription: Subscription
+        }, Y.CustomEvent.DYNAMIC_BASE,
+
+        // Default event
+        '@DEFAULT': new Y.CustomEvent('@DEFAULT', {
+            subscribe: function (target, phase, args) {
+                // Avoid dynamic event routing for white listed DOM events
+                return (Y.Node && Y.Node.NODE_EVENTS[args[0]]) ?
+                    Y.Event.attach(args, (phase === 'capture')) :
+                    this._super.subscribe.apply(this, arguments);
+            },
+
+            unsubscribe: function (target, args) {
+                // TODO
+            },
+
+            // TODO: Remove these if they're not appropriate for the default event
+            Event: DOMEventFacade,
+            Subscription: Subscription
+        }, Y.CustomEvent.DYNAMIC_DEFAULT));
+    },
+
+    /**
+    Publish a synthetic DOM event to add new events encapsulating common
+    subscription patterns or normalize browser inconsistencies. These events
+    can be subscribed to from Y.Nodes or Y.NodeLists, or using `Y.Event.on()`.
+
+    @method publish
+    @param {String|Object} type The name of the event to publish or a map of
+                                configs
+    @param {Object} [config] Behavioral extensions and overrides for this event
+    @param {CustomEvent} [inheritsFrom] Instead of deriving from the class's
+                            default event
+    **/
+    publish: function (type, config, inheritsFrom) {
+        Y.EventTarget._publish(Y.Event, Y.Event._events,
+            type, config, inheritsFrom);
+    },
+
+    /**
+    Subscribe to a DOM event in the bubble phase. See `Y.Event._on()` if
+    you want to subscribe to the capture phase.
+    
+    _type_ may be a string identifying the event or an object map of types to
+    callback functions.
+
+    Custom/Synthetic events may override the default subscription signature, but
+    by default the subscription signature will look like this:
+
+    ```
+    Y.Event.on(type, callback, target, thisObj, extraArg, ...exrtraArgN);
+    ```
+
+    _target_ is optional, but if provided may be a DOM element, array of DOM
+    elements, a Y.Node or Y.NodeList, or selector string. If not provided,
+    `Y.config.win` is used.
+
+    _thisObj_ is optional, and sets the _callback_'s `this` object. The default
+    `this` object in the callback is _target_. Arguments beyond _thisObj_ will
+    be passed to the callback after the event object.
+    
+    If no event published for this class or instance matches the type
+    string exactly, the default event behavior will be used.
+
+    @method on
+    @param {String} type event type to subcribe to
+    @param {Any*} sigArgs see above note on default signature
+    @chainable
+    **/
+    on: function (type) {
+        var event = Y.Event._events[type];
+
+        if (!event) {
+            if (typeof type === STRING) {
+                event = Y.Event._events[DEFAULT];
+            } else {
+                // Defer object/array syntax handling to subscribe()
+                return this.subscribe(toArray(arguments, 0, true));
+            }
+        }
+
+        event.subscribe(this, args);
+
+        return this;
+    },
+
+    /**
+    Subscribe to a DOM event. This method allows capture phase subscriptions.
+    
+    _type_ may be a string identifying the event or an object map of types to
+    callback functions.
+
+    Custom/Synthetic events may override the default subscription signature, but
+    by default the subscription signature will look like this:
+
+    ```
+    Y.Event._on([type, callback, target, thisObj, extraArg, ...exrtraArgN]);
+    ```
+
+    _target_ is optional, but if provided may be a DOM element, array of DOM
+    elements, a Y.Node or Y.NodeList, or selector string. If not provided,
+    `Y.config.win` is used.
+
+    _thisObj_ is optional, and sets the _callback_'s `this` object. The default
+    `this` object in the callback is _target_. Arguments beyond _thisObj_ will
+    be passed to the callback after the event object.
+    
+    If no event published for this class or instance matches the type
+    string exactly, the default event behavior will be used.
+    
+    @method _on
+    @param {Any*} args see above note on default signature
+    @param {Boolean} capture Make the subscription in capture phase if possible
+    @protected
+    **/
+    _on: function (args, capture) {
+        var type  = args[0],
+            event = Y.Event._events[type],
+            i, len;
+
+        if (!event) {
+            if (typeof type === STRING) {
+                event = Y.Event._events[DEFAULT];
+            } else if (isObject(type)) {
+                if (isArray(type)) {
+                    for (i = 0, len = type.length; i < len; ++i) {
+                        args[0] = type[i];
+                        Y.Event._on(args, capture);
+                    }
+                } else {
+                    for (event in type) {
+                        if (type.hasOwnProperty(event)) {
+                            args[0] = event;
+                            // Weak point, assumes signature includes callback
+                            // as third arg for the event (sorta).
+                            args[1] = type[event];
+                            Y.Event._on(args);
+                        }
+                    }
+                }
+
+                return this;
+            }
+        }
+
+        event.subscribe(this, args, capture);
+
+        return this;
+    },
+
+    /**
+    Create a DOM event subscription directly, bypassing any custom or synthetic
+    event logic that may be configured for an event name.
+
+    _args_ is expected to be an array containing:
+    * type (string) - name of the DOM event
+    * callback (function) - subscriber
+    * target (Element|Element[]|Node|NodeList|String) - optional, what to
+        subscribe to
+    * thisObj (object) - overrides `this` in the callback
+    * ...argN - any additional arguments to pass to the callback after the
+        event object.
+
+    @method attach
+    @param {Array} 
+    **/
+    attach: function (args, capture) {
+        var target = args[2],
+            type, callback, i, len, subs, args, eventKey;
+
+        if (!target) {
+            target = Y.config.win;
+        } else if (typeof target === 'string') {
+            target = Y.Selector.queryAll(target);
+
+            if (target.length === 1) {
+                target = target[0];
+            } else if (target.length === 0) {
+                return null;
+            }
+        } else if (Y.Node) {
+            if (target instanceof Y.Node) {
+                target = target._node;
+            } else if (target instanceof Y.NodeList) {
+                target = target._nodes;
+            }
+        }
+
+        // At last, we have a DOM node. Get on with the subscribing, already!
+        if (target.nodeType) {
+            subs     = Y.Event._subs;
+            type     = args[0];
+            eventKey = Y.stamp(target) + ':' + type;
+
+            if (capture) {
+                eventKey += ':capture';
+            }
+
+            if (!subs[eventKey]) {
+                subs[eventKey] =
+                    new Y.Event.DOMSubscription(target, type, capture);
+            }
+
+            return subs[eventKey].subscribe.apply(subs[eventKey], args);
+        } else if (isArray(target)) {
+            type = args[0];
+            subs = [];
+            for (i = 0, len = target.length, i < len; ++i) {
+                args[2] = target[i];
+                subs.push(Y.Event.attach(args, capture));
+            }
+
+            return new Y.Event.Subscription(args[0], null, subs);
+        }
+
+        return null;
+    },
+
+    detach: function (type, callback) {
+        // TODO
+    },
+
+    Subscription: Subscription,
+
+    DOMSubscription: DOMSubscription
+};
+
+}, true);
+
+
+
+
+function DOMEventFacade(type, target, payload) {
     this._event = e;
     this.data   = {};
 }
 Y.extend(DOMEventFacade, EventFacade, {
-    _getter: YObject(EventFacade.prototype._getter, {
+    _getter: {
         target: function () {
             if (!this._target) {
                 var target = this._event.target;
