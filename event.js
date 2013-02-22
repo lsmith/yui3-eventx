414c21ab86f31fa3869a1656edc34d63c187a18f
Luke Smith <lsmith@lucassmith.name>
2013-02-21 16:17:13 -0800

    Consolidate eventx, eventx-dynamic, and eventx-y
    eventx-y was so small, it begged to be included in eventx, but required
    eventx-dynamic, which isn't tiny, but not so big that it convinced me not
    to do it.

--- event.js
+++ event.js
@@ -128,17 +128,6 @@ function CustomEvent(type, config, inheritsFrom) {
     return instance;
 }
 
-/**
-Class to encapsulate custom event subscriptions. This is also assigned to the
-`CustomEvent.prototype` for individual event overrides, but provided statically
-for subclassing.
-
-@property Subscription
-@type {Function}
-@static
-**/
-CustomEvent.Subscription = Subscription;
-
 CustomEvent.prototype = {
     /**
     The class constructor for subscriptions to this event.  Unless the
@@ -581,6 +570,113 @@ CustomEvent.prototype = {
         }
     }
 };
+
+/**
+Class to encapsulate custom event subscriptions. This is also assigned to the
+`CustomEvent.prototype` for individual event overrides, but provided statically
+for subclassing.
+
+@property Subscription
+@type {Function}
+@static
+**/
+CustomEvent.Subscription = Subscription;
+
+// DYNAMIC_BASE and DYNAMIC_DEFAULT would be in a separate module if it weren't
+// for their being useful for Y, and the addition of the EventTarget interface
+// to Y being so small as to make it silly to have split out to a different
+// module. I'd publish them directly on Y if I didn't think they were more
+// widely useful.
+/**
+A base event that includes support for registering dynamic events if they are
+published with a `config.test(target, args)` method or `config.pattern`
+regex to match against the event type string.
+
+Dynamic events are not bound to a specific event name, but use a `test()`
+method to determine if they can appropriately handle a subscription. Two
+examples of dynamic events are category events and routing events.
+
+Category events can be used to handle events whose names match a convention,
+such as 'nameChange' or 'click:li.expandable'.
+
+Routing events can be used to handle a particular subscription signature,
+directing subscriptions to a different subscription mechanism.
+
+By convention, dynamic events should be named starting with an @ symbol
+(e.g. `target.publish('@changes', { pattern: /\w+Change$/, ... });`) and
+should not be explicitly fired. Instead, they are used to create subscriptions
+to other events, possibly publishing them en route.
+
+@property DYNAMIC_BASE
+@type {CustomEvent}
+@static
+**/
+CustomEvent.DYNAMIC_BASE = new Y.CustomEvent('@BASE', {
+    publish: function (target) {
+        var events, defaultEvent;
+
+        if (this.pattern) {
+            this.test = function (target, args) {
+                return this.pattern.test(args[0]);
+            };
+        }
+
+        if (this.test) {
+            events = typeof target === 'function' ?
+                        target.events :
+                        target._yuievt.events;
+            defaultEvent = events['@DEFAULT'];
+
+            if (!defaultEvent.dynamicEvents) {
+                defaultEvent.dynamicEvents = [];
+            }
+
+            if (arrayIndex(defaultEvent.dynamicEvents, this) === -1) {
+                defaultEvent.dynamicEvents.push(this);
+            }
+        }
+    }
+});
+
+/**
+A default event that first looks for published dynamic events to handle
+subscriptions before handing control over to the base event subscription logic.
+
+Dynamic events are not bound to a specific event name, but use a `test()`
+method to determine if they can appropriately handle a subscription. Two
+examples of dynamic events are category events and routing events.
+
+Category events can be used to handle events whose names match a convention,
+such as 'nameChange' or 'click:li.expandable'.
+
+Routing events can be used to handle a particular subscription signature,
+directing subscriptions to a different subscription mechanism.
+
+By convention, dynamic events should be named starting with an @ symbol
+(e.g. `target.publish('@changes', { pattern: /\w+Change$/, ... });`) and
+should not be explicitly fired. Instead, they are used to create subscriptions
+to other events, possibly publishing them en route.
+
+@property DEFAULT_EVENT
+@type {CustomEvent}
+**/
+CustomEvent.DYNAMIC_DEFAULT = new Y.CustomEvent('@DEFAULT', {
+    subscribe: function (target, phase, args) {
+        var events = this.dynamicEvents,
+            event, i, len;
+
+        if (events) {
+            for (i = 0, len = events.length; i < len; ++i) {
+                if (events[i].test(target, args)) {
+                    return events[i].subscribe(target, phase, args);
+                }
+            }
+        }
+
+        // No dynamic event, defer to the base event subscription logic
+        return this._super.subscribe.apply(this, arguments);
+    }
+});
 
 /**
 Class to encapsulate an event subscription. Stores the callback, execution
@@ -1396,5 +1492,14 @@ EventTarget.prototype = {
 Y.CustomEvent = CustomEvent;
 Y.EventFacade = EventFacade;
 Y.EventTarget = EventTarget;
+
+// Add the EventTarget API to Y
+Y.mix(Y, Y.EventTarget.prototype, true);
+Y.EventTarget.call(Y);
+
+Y.publish({
+    '@BASE'   : CustomEvent.DYNAMIC_BASE,
+    '@DEFAULT': CustomEvent.DYNAMIC_DEFAULT
+});
 
 }, '', { requires: ['oop'] });
