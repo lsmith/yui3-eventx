a26d11245fe2a01648bf916e91ce8d6f0142cadd
Luke Smith <lsmith@lucassmith.name>
2013-02-22 16:17:57 -0800

    Event delegation support on EventTarget
    Base implementation is wired for custom events, filtered with a function.
    
    Next up, DOM event implementation.

---
+++ event-delegate.js
@@ -0,0 +1,127 @@
+YUI.add('eventx-delegate', function (Y) {
+/**
+Adds `delegate` method to EventTarget.prototype and a base implementation to
+the CustomEvent.prototype. Event's can override their `delegate` method to
+provide a custom implementation.
+
+@module eventx
+@submodule eventx-delegate
+@for EventTarget
+**/
+var toArray = Y.Array;
+
+/**
+Make a delegated event subscription. The default signature for delegation is:
+
+```
+target.delegate(type, callback, filterFn, thisOverride, ...args);
+```
+
+However, published events can override this signature to look for the filter in
+a different argument position or accept filters in different forms (such as
+selector strings for DOM subscriptions).
+
+_type_ can be a single event name string, an array of event name strings, or an
+object map of event names to callbacks.
+
+@method delegate
+@param {String|String[]|Object} type
+@param {Any} args* See above for default signature
+@return {Subscription}
+**/
+Y.EventTarget.prototype.delegate = function (type) {
+    var event = this._yuievt.events[type],
+        args  = toArray(arguments, 0, true),
+        i, len, subs;
+
+    if (!event) {
+        if (typeof type === 'string') {
+            event = this._yuievt.events['@DEFAULT'];
+        } else if (isObject(type)) {
+            subs = [];
+            if (isArray(type)) {
+                for (i = 0, len = type.length; i < len; ++i) {
+                    args[0] = type[i];
+                    subs.push(this.delegate.apply(this, args));
+                }
+            } else {
+                for (event in type) {
+                    if (type.hasOwnProperty(event)) {
+                        args[0] = event;
+                        // Weak point, assumes signature includes callback
+                        // as second arg for the event (sorta).
+                        args[1] = type[event];
+                        subs.push(this.delegate.apply(this, args));
+                    }
+                }
+            }
+
+            // Batch Subscription
+            return new Y.CustomEvent.Subscription(subs);
+        }
+    }
+
+    return event.delegate ? event.delegate(this, args) : null;
+};
+
+/**
+@for CustomEvent
+**/
+Y.CustomEvent.prototype.delegate = function (target, args) {
+    var subs     = target._yuievt.subs,
+        type     = args[0],
+        callback = args[1],
+        filter   = args[2],
+        sub      = null;
+
+    if (callback && filter) {
+        // Remove the filter replace the callback with the delegation callback
+        args.splice(1, 2, this.delegateFilter);
+
+        sub = new this.Subscription(target, args, 'before', {
+            callback : callback,
+            filter   : filter,
+            container: target,
+
+        }),
+
+        subs = subs[type] || (subs[type] = {});
+        subs = subs['before'] || (subs['before'] = []);
+        subs.push(sub);
+    }
+
+    return sub;
+};
+
+Y.CustomEvent.prototype.delegateFilter = function (e) {
+    var sub     = e.subscription,
+        details = sub && sub.details,
+        filter  = details && details.filter,
+        container, target, defaultThisObj, event, path, i, len;
+
+    if (filter) {
+        container   = details.container;
+        defaultThis = (container === this);
+        target      = e.get('target');
+        path        = target.getEvent(e.type).resolveBubblePath(target);
+
+        e.set('container', container);
+
+        for (i = 0, len = path.length; i < len; ++i) {
+            e.set('currentTarget', path[i]);
+
+            if (filter.call(sub, e)) {
+                // arguments contains e, which has had its currentTarget
+                // updated.
+                details.callback
+                    .apply((defaultThis ? path[i] : this), arguments);
+            }
+
+            if (path[i] === container) {
+                break;
+            }
+        }
+    }
+};
+
+}, '', { requires: ['eventx'] });
