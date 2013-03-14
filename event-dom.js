b4d8fc0076cde103ff20968e4580e0536d5180c5
Luke Smith <lsmith@lucassmith.name>
2013-03-14 13:09:29 -0700

    Jiggering with Facade and other compat changes
    * Remove isEventSupported()
    * Add _getter logic for DOM facade props needing normalization
    * Add ES5 getters/setters for the same for partial back compat
    * Add DOMSubscription class for simpler instantiation and detach(), and proper
      `this` defaulting in notify()
    * Y.on(type, fn, doc.getElementsByTagName(...)) works (DOM collection support)
    * Add Y.Event.purgeElement()

--- event-dom.js
+++ event-dom.js
@@ -2,31 +2,123 @@ YUI.add('eventx-dom', function (Y) {
 
 var isArray = Y.Lang.isArray,
     toArray = Y.Array,
-    push    = Array.prototype.push,
+    slice   = Array.prototype.slice,
 
-    eventTestTags = {
-        select: 'input',
-        change: 'input',
-        submit: 'form',
-        reset : 'form',
-        error : 'img',
-        load  : 'img',
-        abort : 'img'
-    },
-
-    EVENT_NAMES = 'abort beforeunload blur change click close command contextmenu dblclick DOMMouseScroll drag dragstart dragenter dragover dragleave dragend drop error focus key keydown keypress keyup load message mousedown mouseenter mouseleave mousemove mousemultiwheel mouseout mouseover mouseup mousewheel orientationchange reset resize select selectstart submit scroll textInput unload'.split(' '),
+    EVENT_NAMES =
+        ('abort beforeunload blur change click close command contextmenu ' +
+         'dblclick DOMMouseScroll drag dragstart dragenter dragover ' +
+         'dragleave dragend drop error focus key keydown keypress keyup load ' +
+         'message mousedown mouseenter mouseleave mousemove mousemultiwheel ' +
+         'mouseout mouseover mouseup mousewheel orientationchange reset ' +
+         'resize select selectstart submit scroll textInput unload').split(' '),
     DOM_EVENTS = {},
     DOMEvent,
 
-    events, event;
+    webkitKeymap = {
+        63232: 38, // up
+        63233: 40, // down
+        63234: 37, // left
+        63235: 39, // right
+        63276: 33, // page up
+        63277: 34, // page down
+        25:     9, // SHIFT-TAB (Safari provides a different key code in
+                   // this case, even though the shiftKey modifier is set)
+        63272: 46, // delete
+        63273: 36, // home
+        63275: 35  // end
+    },
+
+    FACADE_PROPS = ('relatedTarget container ' +
+                    'altKey ctrlKey metaKey shiftKey ' +
+                    'clientX clientY pageX pageY ' +
+                    'keyCode charCode which button wheelDelta').split(' '),
+
+    events, name, i, len;
+
 
 function DOMEventFacade(type, target, payload) {
+    var key;
+
+    this.type   = type;
+    this.data   = {
+        type         : type,
+        target       : target
+    };
     this._event = payload._event;
 
-    Y.EventFacade.apply(this, arguments);
+    for (key in payload) {
+        if (payload.hasOwnProperty(key)) {
+            this.data[key] = payload[key];
+        }
+    }
+
+    // Override getters for specific event types
+    // TODO
+    /*
+    if (DOMEventFacade._typeGetters[type]) {
+        this._getter = DOMEventFacade._typeGetters[type];
+    }
+    */
+}
+
+function getKeyCode() {
+    var e, code;
+
+    // Because the same code is used for e.keyCode, e.charCode, and e.which
+    if (!this.data.keyCode) {
+        e    = this._event;
+        code = e.keyCode || e.charCode;
+
+        this.data.keyCode = webkitKeymap[code] || code;
+    }
+
+    return this.data.keyCode;
+}
+
+function getWhich() {
+    return this._event.which ||
+           this._event.charCode ||
+           this.get('keyCode'); // fall back to the getter for keyCode
 }
 
 Y.extend(DOMEventFacade, Y.EventFacade, {
+    // Don't share the getters object from Y.EventFacade or event-node will
+    // add _getter.target for all events, custom and DOM.
+    // TODO: should this be Y.Object(EF.proto._getter)?
+    _getter: {
+        // charCode is unknown in keyup, keydown. keyCode is unknown in keypress.
+        // FF 3.6 - 8+? pass 0 for keyCode in keypress events.
+        // Webkit, FF 3.6-8+?, and IE9+? pass 0 for charCode in keydown, keyup.
+        // Webkit and IE9+? duplicate charCode in keyCode.
+        // Opera never sets charCode, always keyCode (though with the charCode).
+        // IE6-8 don't set charCode or which.
+        // All browsers other than IE6-8 set which=keyCode in keydown, keyup, and
+        // which=charCode in keypress.
+        //
+        // Moral of the story: (e.which || e.keyCode) will always return the
+        // known code for that key event phase. e.keyCode is often different in
+        // keypress from keydown and keyup.
+        keyCode : getKeyCode,
+        charCode: getKeyCode,
+        which   : getWhich,
+        button  : getWhich,
+        wheelDelta: function () {
+            var e;
+
+            if (!('wheelDelta' in this.data)
+            && (this.type === 'mousewheel' || this.type === 'DOMMouseScroll')) {
+                e = this._event;
+
+                this.data.wheelData = ((e.detail) ?
+                    (e.detail * -1) :
+                    Math.round(e.wheelDelta / 80)) ||
+                        ((e.wheelDelta < 0) ? -1 : 1);
+            }
+
+            return this.data.wheelDelta;
+        }
+    },
+
     /**
     Get a property from the event's data collection supplied at event creation.
     Returns one of the following, in priority order:
@@ -39,8 +131,6 @@ Y.extend(DOMEventFacade, Y.EventFacade, {
     @param {String} name Data property name
     @return {Any} whatever is stored in the data property
     **/
-    // TODO: node-event module to add _getter for target, currentTarget, and
-    // relatedTarget and deal with target possibly being a text node
     get: function (name) {
         if (this._getter[name]) {
             return this._getter[name].call(this, name);
@@ -58,7 +148,7 @@ Y.extend(DOMEventFacade, Y.EventFacade, {
     @chainable
     **/
     preventDefault: function () {
-        this._prevented = true;
+        this.prevented = true;
 
         if (this._event.preventDefault) {
             this._event.preventDefault();
@@ -81,8 +171,8 @@ Y.extend(DOMEventFacade, Y.EventFacade, {
     **/
     stopPropagation: function () {
         // It might have been stopped with 2 already
-        if (!this._stopped) {
-            this._stopped = 1;
+        if (!this.stopped) {
+            this.stopped = 1;
 
             if (this._event.stopPropagation) {
                 this._event.stopPropagation();
@@ -105,7 +195,7 @@ Y.extend(DOMEventFacade, Y.EventFacade, {
     @chainable
     **/
     stopImmediatePropagation: function () {
-        this._stopped = 2;
+        this.stopped = 2;
 
         if (this._event.stopPropagation) {
             this._event.stopPropagation();
@@ -131,26 +221,99 @@ Y.extend(DOMEventFacade, Y.EventFacade, {
 
         return this;
     }
+}, {
+    /**
+    Map of getters for specific event types, such as mouse, key, or touch
+    events, to avoid misapplied overhead from events other than the type
+    needing the extra logic.
+
+    @property _typeGetters
+    @type {Object}
+    @static
+    @protected
+    **/
+    _typeGetters: {}
+});
+
+// Add getter/setter for common properties to allow e.shiftKey rather than
+// e.get('shiftKey'), even though the latter would be faster (probably).
+if (Object.defineProperties) { // definePropertIES to avoid IE8's bustedness
+    Y.Array.each(FACADE_PROPS, function (prop) {
+        Object.defineProperty(DOMEventFacade.prototype, prop, {
+            get: function () { return this.get(prop); },
+            set: function (val) { this.set(prop, val); }
+        });
+    });
+}
+
+function DOMSubscription(target, args, phase, details) {
+    // Does not support batch construction
+    this.target   = target;
+    this.phase    = phase;
+    this.details  = details;
+
+    this.type     = args[0];
+    this.callback = args[1];
+    this.thisObj  = args[2];
+
+    if (args.length > 3) {
+        this.payload = slice.call(args, 3);
+    }
+}
+
+// extends for instanceof support, but overrides both methods (yay, instanceof!)
+Y.extend(DOMSubscription, Y.CustomEvent.Subscription, {
+    /**
+    Call the subscribed callback with the provided event object, followed by
+    any bound subscription arguments.
+
+    @method notify
+    @param {Event.EventFacade} e DOMEventFacade to pass to callback
+    **/
+    notify: function (e) {
+        var thisObj  = this.thisObj || e.get('currentTarget'),
+            callback = this.callback;
+
+        // Unfortunate cost of back compat. Would rather deprecate once and
+        // onceAfter in favor of e.detach().
+        if (this.once) {
+            this.detach();
+        }
+
+        return this.payload ?
+            callback.apply(thisObj, [e].concat(this.payload)) :
+            callback.call(thisObj, e);
+    },
+
+    /**
+    Detaches the subscription from the subscribed target. Convenience for
+    `Y.detach(this);`.
+
+    @method detach
+    **/
+    detach: function () {
+        Y.detach(this);
+    }
 });
 
 DOMEvent = new Y.CustomEvent('@dom-event', {
     subscribe: function (target, args, phase) {
         var type    = args[0],
             capture = (phase === 'capture'),
-            phase   = capture ? 'capture' : 'on',
             sub     = null,
             el, eventKey, subs, i, len;
 
-        // Convert from arguments object to array
-        args = toArray(args, 0, true);
-
         if (target === Y) {
+            // Convert from arguments object to array
+            args = toArray(args, 0, true);
             target = args.splice(2,1)[0];
         }
 
         el = Y.Event._resolveTarget(target);
 
         if (el && el.nodeType) {
+            phase = capture ? 'capture' : 'on';
+
             eventKey = args[0] = Y.stamp(el) + ':' + type;
 
             sub = new this.Subscription(Y, args, phase, {
@@ -158,10 +321,11 @@ DOMEvent = new Y.CustomEvent('@dom-event', {
             });
 
             this.registerSub(el, sub);
-        // TODO: el could be a DOM collection ala getElementsByTagName()
-        } else if (isArray(el)) {
+        } else if (el && el.length) {
             subs = [];
             for (i = 0, len = el.length; i < len; ++i) {
+                // args[0] is assigned the eventKey. Need to reset it.
+                args[0] = type;
                 subs.push(this.subscribe(el[i], args, phase));
             }
 
@@ -193,7 +357,6 @@ DOMEvent = new Y.CustomEvent('@dom-event', {
     unsubscribe: function (target, args) {
         var type  = args[0],
             isSub = (type.type && type.callback),
-            isY   = (target === Y),
             el, eventKey, capture, phase, i, len, subs;
 
         if (isSub) {
@@ -201,6 +364,7 @@ DOMEvent = new Y.CustomEvent('@dom-event', {
         }
 
         if (target === Y) {
+            args   = toArray(args, 0, true);
             target = args.splice(2, 1)[0];
         }
 
@@ -219,21 +383,20 @@ DOMEvent = new Y.CustomEvent('@dom-event', {
             if (!subs[eventKey] || !subs[eventKey][phase]) {
                 YUI.Env.remove(el, type, Y.Event._handleEvent, capture);
             }
-        } else if (isArray(el)) {
+        } else if (el && el.length) {
             for (i = 0, len = el.length; i < len; ++i) {
                 this.unsubscribe(el[i], args);
             }
         }
     },
 
-    fire: function (target, type, e, currentTarget) {
-        var type     = e.type,
-            eventKey = Y.stamp(currentTarget) + ':' + type,
-            phase    = (e.eventPhase === 1) ? 'capture' : 'on',
+    fire: function (target, type, event, currentTarget) {
+        var eventKey = Y.stamp(currentTarget) + ':' + type,
+            phase    = (event.eventPhase === 1) ? 'capture' : 'on',
             subs     = Y._yuievt.subs[eventKey],
-            event, i, len, sub, ret;
+            e, i, len, sub, ret;
 
-        subs  = subs && subs[phase];
+        subs = subs && subs[phase];
 
         // This shouldn't happen because the last detached sub should
         // trigger the DOM sub being removed, but just in case...
@@ -241,55 +404,40 @@ DOMEvent = new Y.CustomEvent('@dom-event', {
             return;
         }
 
-        event = new this.Event(type,
-                    (e.target || e.srcElement), {
-                        _event       : e,
-                        currentTarget: currentTarget
-                    });
-
-        // To be set using event.get('currentTarget');
-        currentTarget = null;
+        e = new this.Event(type, (event.target || event.srcElement), {
+            _event       : event,
+            currentTarget: currentTarget
+        });
 
         for (i = 0, len = subs.length; i < len; ++i) {
             sub = subs[i];
 
-            event.subscription = sub;
+            e.subscription = sub;
 
-            if (sub.thisObj) {
-                ret = sub.notify(event);
-            } else {
-                // Lazy evaluate the current target as the thisObj
-                // for the subscription to avoid the function call and
-                // potential getter cost to create a Node instance
-                sub.thisObj = currentTarget ||
-                        (currentTarget = event.get('currentTarget'));
+            ret = sub.notify(e);
 
-                sub.notify(event);
-
-                sub.thisObj = null;
-            }
-
-            event.subscription = null;
+            e.subscription = null;
 
             // Boy I hate this "feature"
             if (ret === false) {
-                event.halt();
+                e.halt();
             }
 
-            if (event._stopped === 2) {
+            if (e.stopped === 2) {
                 break;
             }
         }
     },
 
-    Event: DOMEventFacade
+    Event       : DOMEventFacade,
+
+    Subscription: DOMSubscription
 }, Y.CustomEvent.FacadeEvent);
 
 for (i = 0, len = EVENT_NAMES.length; i < len; ++i) {
     DOM_EVENTS[EVENT_NAMES[i]] = DOMEvent;
 }
 
-// Y.Event is an EventTarget
 Y.Event = {
     /**
     DOM and synthetic event collection for sharing between Y, Node, and
@@ -329,42 +477,68 @@ Y.Event = {
                             customizations in _config_ are applied
     **/
     publish: function (type, config, inheritsFrom, smart) {
-        // Default DOMEvent as base so Y.Event.publish('beforeunload')
-        // is enough to whitelist a DOM event
-        inheritsFrom || (inheritsFrom = Y.Event.DOMEvent);
-
-        if (config || inheritsFrom !== Y.Event.DOMEvent) {
+        // Default DOMEvent for Y.Event.publish('beforeunload') use case to
+        // make it easy to whitelist existing DOM events. Don't default
+        // DOMEvent for others because synthetic events have custom behavior
+        // and should relate to DOMEvent behavior only on an as-needed basis,
+        // and likely via the EventTarget API on Node/Y.
+        if (arguments.length === 1) {
+            Y.Event.DOM_EVENTS[type] = Y.Event.DOMEvent;
+        } else {
             Y.EventTarget._publish(Y.Event, Y.Event.DOM_EVENTS,
                 type, config, inheritsFrom, smart);
-        } else if (!Y.Event.DOM_EVENTS[type]) {
-            Y.Event.DOM_EVENTS[type] = inheritsFrom;
         }
     },
 
     /**
-    Tests if an event name is a DOM event.
+    Remove all DOM event subscriptions from _target_, optionally also purging
+    subscriptions of _target_'s subtree. Specify a _type_ to limit detaching to
+    a specific event.
 
-    @method isEventSupported
-    @param {String} type
-    @return {Boolean}
+    _target_ can be a selector string, Node, NodeList, HTMLElement, or array
+    of HTMLElements.
+
+    @method purgeElement
+    @param {String|Node|NodeList|Element|Element[]} target What to purge
+    @param {Boolean} [recurse] If truthy, also purge all descendants
+    @param {String} [type] Limit purge to this event
     **/
-    //Code by kangax, from 
-    //http://perfectionkills.com/detecting-event-support-without-browser-sniffing/
-    // TODO: Remove? This is no longer needed.
-    isEventSupported: Y.cached(function (type) {
-        var el = Y.config.doc.createElement(eventTestTags[type] || 'div'),
-            eventName   = 'on' + type;
-            isSupported = (eventName in el);
+    purgeElement: function (target, recurse, type) {
+        var yuid, i, len, subs, eventKey;
 
-        if (!isSupported) {
-            el.setAttribute(eventName, 'return;');
-            isSupported = typeof el[eventName] === 'function';
+        target = Y.Event._resolveTarget(target);
+
+        if (target) {
+            if (target.nodeType) {
+                yuid = Y.stamp(target);
+
+                if (recurse) {
+                    // purge children, but don't return because we still need
+                    // to purge the target
+                    Y.Event.purgeElement(
+                        target.getElementsByTagName('*'), false, type);
+                }
+
+                if (type) {
+                    Y.detach(type, null, target);
+                } else {
+                    subs = Y._yuievt.subs;
+
+                    for (eventKey in subs) {
+                        if (eventKey.indexOf(yuid) === 0) {
+                            Y.detach(
+                                eventKey.slice(eventKey.lastIndexOf(':') + 1),
+                                null, target);
+                        }
+                    }
+                }
+            } else if (target.length) {
+                for (i = 0, len = target.length; i < len; ++i) {
+                    Y.Event.purgeElement(target[i], recurse, type);
+                }
+            }
         }
-
-        el = null;
-
-      return isSupported;
-    }),
+    },
 
     /**
     Resolves the input value to a DOM Element or array of DOM Elements.
@@ -374,7 +548,7 @@ Y.Event = {
     If no target is passed, `Y.config.win` is returned.
 
     @method _resolveTarget
-    @param {String|Node|NodeList|Element|Element[]}
+    @param {String|Node|NodeList|Element|Element[]} target
     @return {Element|Element[]}
     @protected
     **/
@@ -432,10 +606,10 @@ Y.Event = {
 events = Y._yuievt.events;
 Y._yuievt.events = Y.Object(Y.Event.DOM_EVENTS);
 
-for (event in events) {
+for (name in events) {
     // ignore hasOwnProperty in favor of duck typing CustomEvent or Router
-    if (events[event].subscribe) {
-        Y._yuievt.events[event] = events[event];
+    if (events[name].subscribe) {
+        Y._yuievt.events[name] = events[name];
     }
 }
 
